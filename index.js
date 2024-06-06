cfec1609c8ceee76f566752d4fa9d4dbb0868bd3
Rodney St Cloud <hoyrod1@aol.com>
2024-06-05 22:59:21 -0400

    The index,js file was created as the entry point to the application, the .en file was created to store the connection credentials for the application, the express server, nodeman and mongoose was installes as dependencies

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+//====================================================================//
+const express = require("express");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+//====================================================================//
+const app = express();
+//====================================================================//
+const PORT = process.env.PORT || 8000;
+//====================================================================//
+
+//====================================================================//
+app.use(express.json());
+//====================================================================//
+app.use((req, res, next) => {
+  console.log(`A ${req.method} request was made to ${req.url}`);
+  next();
+});
+//====================================================================//
+app.get("/", (request, response) => {
+  response.send("Welcome To My First Express Application");
+});
+//====================================================================//
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
